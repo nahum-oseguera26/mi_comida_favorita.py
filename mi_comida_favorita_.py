b2eea8d7e7ad0ccb31704a35416958ac3f5a0d38
nahum-oseguera26 <49953527+nahum-oseguera26@users.noreply.github.com>
2019-06-05 09:11:50 -0600

    Add files via upload

---
+++ mi_comida_favorita_.py
@@ -0,0 +1,8 @@
+comidas = ["Baleadas", "sopa marinera", "Pezcado Frito", "pastelitos", "esparragos", "Espagueti", "Pollo Asado", "pechuga en salsa de hongo", "sopa de gallina", "Pupusas", "huevo con tocino", "Sopa De Res", "Sopa De Pollo", "sopa de caracol", "Sopa De Frijoles", "pollo chuco", "Pollo A La Naranja", "Pinchos De Chorizo", "Chorizo A La Parilla", "Caracol A La Plancha"]
+
+def imprimir_menu()
+    print("Escoja sus platillos favoritos")
+    for comida in comida
+        print(comida)
+
+imprimir_menu()
